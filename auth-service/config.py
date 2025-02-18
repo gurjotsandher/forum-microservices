4888c8a1c42b16a1535287406924ca47b279bad4
gurjotsandher <gurjotsa143@gmail.com>
2025-02-18 12:00:14 -0800

    Reset migrations and updated configurations structure

--- auth-service/config.py
+++ auth-service/config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     """Base config class."""
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    # JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     DB_SERVICE_URL = os.getenv("DB_SERVICE_URL")
 
--- db-service/config.py
+++ db-service/config.py
@@ -1,9 +1,8 @@
 import os
-import requests
 
 class Config:
     """Base config class for db-service."""
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = None # Temporary hardcoded URL for migrations
-    CACHE_TYPE = "RedisCache"
+    SQLALCHEMY_DATABASE_URI = os.getenv("MIGRATION_URI")
+    CACHE_TYPE = os.getenv("CACHE_TYPE") # "RedisCache"
     CACHE_REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
